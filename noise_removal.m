b53170ee8c17cecc977ad8c93f61f7b037dc6d52
alidarbandi <alidarbandi@users.noreply.github.com>
2018-02-21 10:57:13 -0700

    described file

---
+++ noise_removal.m
@@ -0,0 +1,12 @@
+% clear all;
+img2=imread('test\BF-9.tif'); %insert the addres of the image you would like to process
+
+hsize=19;   %these are the parameters of the Gaussian filtering. Read help for details
+sigma=5;    % I put some default value here but feel free to change until best image is obtained
+h=fspecial('gaussian',hsize,sigma);
+% figure, surf(h); %if you uncomment this, it will plot the filter for you
+outp=imfilter(img2,h);
+ figure,imshow(outp);  %processed image
+% saveas(gcf,'test\BF2.tif');
+ %imcontrast(gca);      %contrast tool
+ figure, imshow(img2); %original imag
